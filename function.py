637d8697ea9971b52a793a104515909f54a1c30b
RazibBP <mdrazib.dev@gmail.com>
2025-04-24 23:01:01 +0600

    basic function code

--- function.py
+++ function.py
@@ -8,3 +8,23 @@ happy_brithday()
 happy_brithday()
 happy_brithday()
 
+def add(x,y):
+    sum = x + y
+    print(sum)
+
+def subtraction(x,y):
+    sum = x - y
+    print(sum)
+
+def maltiplication(x , y):
+    mal = x * y
+    print(mal)
+
+def division(x , y):
+    div = x / y
+    print(div)
+
+add(10,20)
+subtraction(30,60)
+division(10,2)
+maltiplication(10,10)
