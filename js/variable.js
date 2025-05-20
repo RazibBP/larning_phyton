5f3d6b400fbac59b5e0b82676d3ed7916b096cd8
RazibBP <mdrazib.dev@gmail.com>
2025-05-20 15:55:53 +0600

    javascript_html

---
+++ js/variable.js
@@ -0,0 +1,17 @@
+function myfunction(){
+    document.getElementById("demo").innerHTML = "that is changed"
+}
+
+
+var x = 6
+var y = 7
+var z = 8
+var i = x + y + z
+console.log(i)
+
+{
+let x = 6
+let y = 7
+let z = x + y
+console.log(z)
+}
